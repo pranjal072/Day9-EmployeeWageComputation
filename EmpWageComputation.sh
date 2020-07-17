d05d912cabc18ad2bd86e9d323839cf16717dc40
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-17 19:05:24 +0530

    Employee Attendance Added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
-
-echo "Welcome to Employee Wage Computation Program on Master Branch"
+Attendance=$(( RANDOM%2 ))
+if [ $Attendance -eq 1 ]
+then
+   echo "Employee is Present"
+else
+   echo "Employee is Absent"
+fi
