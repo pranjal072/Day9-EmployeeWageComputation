53c3144e28ec0fd012934b1e48c30e4e19ed5c2a
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-18 14:03:42 +0530

    Daily Wage added at 20 Rs per Hour if Present

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,10 +2,17 @@
 
 echo "Welcome to Employee Wage Computation Program on Master Branch:"
 
+RatePerHr=20
 Attendance=$(( RANDOM%2 ))
 if [ $Attendance -eq 1 ]
 then
    echo "Employee is Present"
+	WorkingHr=8
 else
    echo "Employee is Absent"
+	WorkingHr=0
 fi
+
+Salary=$(( $WorkingHr*$RatePerHr ))
+
+echo "Salary per day is : $Salary"
