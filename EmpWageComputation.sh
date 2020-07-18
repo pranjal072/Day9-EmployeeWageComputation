9b767a5c60b42d9e9a66d5cf30dd32e6f1d955ac
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-18 14:16:45 +0530

    Part time work added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program on Master Branch:"
-
+fullTime=1
+partTime=2
 RatePerHr=20
-Attendance=$(( RANDOM%2 ))
-if [ $Attendance -eq 1 ]
+Attendance=$(( RANDOM%3 ))
+if [ $Attendance -eq $fullTime ]
 then
-   echo "Employee is Present"
+	echo "Employee is working Full Time"
 	WorkingHr=8
+elif [ $Attendance -eq $partTime ]
+then
+	echo "Employee is working Part Time"
+	WorkingHr=4
 else
    echo "Employee is Absent"
 	WorkingHr=0
