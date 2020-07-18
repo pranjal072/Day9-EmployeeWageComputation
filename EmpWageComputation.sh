6f82b3845c86af66fde9de630cb15e29ef8d8108
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-18 14:41:09 +0530

    Exchanged if else with Switch Case

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,22 +1,24 @@
 #!/bin/bash -x
 
-echo "Welcome to Employee Wage Computation Program on Master Branch:"
+echo "Welcome to Employee Wage Computation Program:"
 fullTime=1
 partTime=2
 RatePerHr=20
 Attendance=$(( RANDOM%3 ))
-if [ $Attendance -eq $fullTime ]
-then
-	echo "Employee is working Full Time"
-	WorkingHr=8
-elif [ $Attendance -eq $partTime ]
-then
-	echo "Employee is working Part Time"
-	WorkingHr=4
-else
-   echo "Employee is Absent"
-	WorkingHr=0
-fi
+case $Attendance in
+	$fullTime)
+		echo "Employee is working Full Time"
+		WorkingHr=8
+		;;
+	$partTime)
+		echo "Employee is working Part Time"
+		WorkingHr=4
+		;;
+	*)
+		echo "Employee is Absent"
+		WorkingHr=0
+		;;
+esac
 
 Salary=$(( $WorkingHr*$RatePerHr ))
 
