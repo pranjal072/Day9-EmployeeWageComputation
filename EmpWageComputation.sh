bb66188085275caeca5d084ff03ba8a4bc83551e
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-18 15:12:25 +0530

    Monthly wage added for 20 days a month

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,22 +4,26 @@ echo "Welcome to Employee Wage Computation Program:"
 fullTime=1
 partTime=2
 RatePerHr=20
-Attendance=$(( RANDOM%3 ))
-case $Attendance in
-	$fullTime)
-		echo "Employee is working Full Time"
-		WorkingHr=8
-		;;
-	$partTime)
-		echo "Employee is working Part Time"
-		WorkingHr=4
-		;;
-	*)
-		echo "Employee is Absent"
-		WorkingHr=0
-		;;
-esac
+WorkingDaysPerMonth=20
+for((i=0; i<$WorkingDaysPerMonth; i++))
+do
+	Attendance=$(( RANDOM%3 ))
+	case $Attendance in
+		$fullTime)
+			echo "Employee is working Full Time"
+			WorkingHr=8
+			;;
+		$partTime)
+			echo "Employee is working Part Time"
+			WorkingHr=4
+			;;
+		*)
+			echo "Employee is Absent"
+			WorkingHr=0
+			;;
+	esac
+	Salary=$(( $WorkingHr*$RatePerHr ))
+	TotalSalary=$(($TotalSalary+$Salary))
+done
 
-Salary=$(( $WorkingHr*$RatePerHr ))
-
-echo "Salary per day is : $Salary"
+echo "Salary per month is : $TotalSalary"
